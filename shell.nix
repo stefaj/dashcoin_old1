bca24e50e88bb602f5403d4826126432b9bddfeb
Stefan Jacholke <stefanjacholke@gmail.com>
2017-05-24 22:20:27 +0200

    Update shell nix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
     name = "hack";
-    buildInputs = [ boost155
-                    gcc49
-                    cmake_2_8
+    buildInputs = [ boost
+                    gcc
+                    cmake
                    ]; 
 
 }
